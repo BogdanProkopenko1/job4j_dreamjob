976a74157343572fedbf12738a8704881ccddf2f
BogdanProkopenko1 <integer.args@gmail.com>
2021-08-20 22:00:18 +0500

    1. Написать тест на PostServlet. [#2518]

---
+++ src/main/java/ru/job4j/dream/servlet/CandidateServletTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.dream.servlet;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.store.Store;
+import ru.job4j.dream.store.PsqlStore;
+import ru.job4j.dream.store.MemStore;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PsqlStore.class)
+public class CandidateServletTest {
+
+    @Test
+    public void whenCreatePost() throws IOException {
+        Store store = MemStore.instOf();
+
+        PowerMockito.mockStatic(PsqlStore.class);
+        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
+
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+
+        PowerMockito.when(req.getParameter("id")).thenReturn("0");
+        PowerMockito.when(req.getParameter("name")).thenReturn("n");
+        PowerMockito.when(req.getParameter("skills")).thenReturn("s");
+        PowerMockito.when(req.getParameter("position")).thenReturn("p");
+
+        new CandidateServlet().doPost(req, resp);
+
+        Candidate result = store.findAllCandidates().iterator().next();
+        Assert.assertThat(result.getName(), Is.is("n"));
+        Assert.assertThat(result.getSkills(), Is.is("s"));
+        Assert.assertThat(result.getPosition(), Is.is("p"));
+    }
+}
