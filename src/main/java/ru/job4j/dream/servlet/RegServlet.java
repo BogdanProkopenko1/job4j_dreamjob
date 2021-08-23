d93e7010e461dcc6aa6ef5d060ab8f876628f37a
BogdanProkopenko1 <integer.args@gmail.com>
2021-08-23 23:22:35 +0500

    4. Регистрация пользователя. [#283110 #192191]

--- src/main/java/ru/job4j/dream/servlet/RegServlet.java
+++ src/main/java/ru/job4j/dream/servlet/RegServlet.java
@@ -3,6 +3,7 @@ package ru.job4j.dream.servlet;
 import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.PsqlStore;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,27 +13,23 @@ import java.io.IOException;
 public class RegServlet extends HttpServlet {
 
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         User user = new User(
                 0,
                 req.getParameter("name"),
                 req.getParameter("email"),
                 req.getParameter("password")
         );
-        User fromDB = PsqlStore.instOf().getUserOnEmail(user.getEmail());
-        boolean bool = fromDB != null;
-        HttpSession sc = req.getSession();
-        if (user.finished() && fromDB == null) {
+        boolean exist = PsqlStore.instOf().getUserOnEmail(user.getEmail()) != null;
+        if (exist) {
+            req.setAttribute("error", "Account already exist");
+            req.getRequestDispatcher("reg.jsp").forward(req, resp);
+        } else {
+            HttpSession sc = req.getSession();
             PsqlStore.instOf().save(user);
             sc.setAttribute("user", user);
             sc.setAttribute("username", user.getName());
             resp.sendRedirect(req.getContextPath() + "/posts.do");
-        } else if (bool && user.getEmail().equals(fromDB.getEmail())) {
-            req.setAttribute("error", "Account already exist");
-            resp.sendRedirect(req.getContextPath() + "/login.jsp");
-        } else {
-            req.setAttribute("error", "Incorrect data entered");
-            resp.sendRedirect(req.getContextPath() + "/reg.jsp");
         }
     }
 }
